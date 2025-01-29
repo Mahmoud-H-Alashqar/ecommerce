595013ddc1578a15919679a66f1da018f8e118a5
mahmoud1994-2024 <m.ashqar@epicsyst.com>
2025-01-28 17:05:05 -0800

    eee

--- resources/views/website/frontend/index.blade.php
+++ resources/views/website/frontend/index.blade.php
@@ -33,7 +33,32 @@
     color: white;
 }
 
-	   
+	   /*  توحيد طول الديف للممنتجات*/
+       /* توحيد طول الـ div الخاصة بالمنتجات */
+.fruite-item {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+}
+
+/* توحيد طول الصور بحيث تكون متساوية الحجم */
+.fruite-img img {
+    object-fit: cover; /* لضمان تغطية الصورة بالكامل للحاوية */
+    height: 200px; /* يمكن تغيير هذا الرقم حسب الطول الذي تريده */
+}
+
+/* توحيد أبعاد الحاويات */
+.col-md-6, .col-lg-4, .col-xl-3 {
+    display: flex;
+    justify-content: stretch;
+    flex-wrap: wrap;
+}
+
+/* التأكد من أن كل منتج له نفس الارتفاع */
+.fruite-item .p-4 {
+    flex-grow: 1; /* يضمن أن الجزء الذي يحتوي على النصوص سيأخذ المساحة المتبقية */
+}
+
 	   </style>
        <!-- Modal Search Start -->
         <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -213,7 +238,7 @@
   <div id="tab-all" class="tab-pane fade show p-0 active">
         <div class="row g-4">
             <!-- هنا يمكن إضافة جميع المنتجات -->
-            @foreach($products as $product) <!-- تأكد من أنك تمرر جميع المنتجات إلى الواجهة -->
+            {{--@foreach($products as $product) <!-- تأكد من أنك تمرر جميع المنتجات إلى الواجهة -->
             <div class="col-md-6 col-lg-4 col-xl-3">
                 <div class="rounded position-relative fruite-item">
                     <div class="fruite-img">
@@ -232,7 +257,28 @@
                     </div>
                 </div>
             </div>
-            @endforeach
+            @endforeach--}}
+            @foreach($products as $product)
+<div class="col-md-6 col-lg-4 col-xl-3">
+    <div class="rounded position-relative fruite-item">
+        <div class="fruite-img">
+            <img src="{{ asset('storage/' . $product->image) }}" class="img-fluid w-100 rounded-top" alt="">
+        </div>
+        <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">{{ $product->category->name }}</div>
+        <div class="p-4 border border-secondary border-top-0 rounded-bottom">
+            <h4>{{ $product->name }}</h4>
+            <p>{{ $product->description }}</p>
+            <div class="d-flex justify-content-between flex-lg-wrap">
+                <p class="text-dark fs-5 fw-bold mb-0">${{ $product->price }}  </p>
+                <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary">
+                    <i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
+
         </div>
 		    <!-- روابط الـ Pagination -->
        <div class="d-flex justify-content-center mt-4">
